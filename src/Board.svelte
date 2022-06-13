8e7a6bfbbcc9accf289ce99e6bd9301e68431060
World Smiths <worldsmithsvtt@gmail.com>
2022-06-13 14:50:02 +0000

    feat: utilise un modèle de pawn

--- src/Board.svelte
+++ src/Board.svelte
@@ -4,32 +4,36 @@
     import * as PE from "svelte-cannon";
     import * as THREE from "three";
     import * as SC from "svelte-cubed";
-    import DirecionalPerspectiveCamera from "./custom/DirectionalPerspectiveCamera.svelte";
+    import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+    import DirectionalPerspectiveCamera from "./custom/DirectionalPerspectiveCamera.svelte";
     import ContactMaterial from "./custom/ContactMaterial.svelte";
     import Window from "./Window.svelte";
 
     export let socket: Socket;
-    let width = 1,
-        height = 1,
-        depth = 1,
-        color = "red",
-        position = PE.writableVec3(0, 0, 0),
-        direction: THREE.Vector3 & CANNON.Vec3,
-        quadrant = 0;
 
-    $: quadrant = Math.round(((Math.atan2(direction?.x, direction?.z) * 180) / Math.PI + 180 + 45) / 90);
+    let grandeur = 1,
+        color: string = "#8a0000",
+        position = PE.writableVec3(0, 0, 0),
+        direction: THREE.Vector3 & CANNON.Vec3;
 
     const keys: Set<string> = new Set();
     function keyDown(event: KeyboardEvent) {
         const { key } = event;
+        if (document.activeElement?.matches("input")) return;
         keys.add(key);
 
-        const { x, y, z } = $position;
-        if (keys.has("ArrowLeft")  || keys.has("a"))  $position = $position.addScaledVector(1, new CANNON.Vec3(direction.z, 0, -direction.x));
-        if (keys.has("ArrowRight") || keys.has("d"))  $position = $position.addScaledVector(-1, new CANNON.Vec3(direction.z, 0, -direction.x));
-        if (keys.has(" "))       if ($position.y < 1) $position = $position.addScaledVector(1, new CANNON.Vec3(0, 1, 0));
-        if (keys.has("ArrowUp")    || keys.has("w"))  $position = $position.addScaledVector(1, new CANNON.Vec3(direction.x, 0, direction.z));
-        if (keys.has("ArrowDown")  || keys.has("s"))  $position = $position.addScaledVector(-1, new CANNON.Vec3(direction.x, 0, direction.z));
+        // Ajoute 180 car l'angle est entre -180 et 180
+        const quadrant = Math.round(((Math.atan2(direction?.x, direction?.z) * 180) / Math.PI + 90) / 90),
+            SIN = Math.sin(quadrant * Math.PI / 2),
+            COS = Math.cos(quadrant * Math.PI / 2);
+        
+        let Δ = new CANNON.Vec3();
+        if (keys.has("ArrowLeft")  || keys.has("a")) Δ.set(SIN, 0, COS);
+        if (keys.has("ArrowRight") || keys.has("d")) Δ.set(-SIN, 0, -COS);
+        if (keys.has("ArrowUp")    || keys.has("w")) Δ.set(-COS, 0, SIN);
+        if (keys.has("ArrowDown")  || keys.has("s")) Δ.set(COS, 0, -SIN);
+        if (keys.has(" ") && Number($position.y.toFixed(3)) == 0) Δ.set(0, 1, 0);
+        socket.emit("mouvement", Δ);
     }
 
     function keyUp(event: KeyboardEvent) {
@@ -37,17 +41,24 @@
         keys.delete(key);
     }
 
-    const loader = new THREE.TextureLoader();
-    const floorTexture = loader.load("assets/floor.jpg");
+    // Texture du sol
+    const floorLoader = new THREE.TextureLoader();
+    const floorTexture = floorLoader.load("assets/floor.jpg");
     floorTexture.wrapS = THREE.RepeatWrapping;
     floorTexture.wrapT = THREE.RepeatWrapping;
     floorTexture.repeat.set(10, 10);
 
-    $: socket.timeout(5000).emit("mouvement", $position, (err: Error) => {
-        if (err) {
-            console.log(err);
+    // Model 3d du pawn
+    const objLoader = new GLTFLoader();
+    let model: THREE.BufferGeometry;
+    objLoader.load(
+        "assets/pawn.glb",
+        ({scene}) => {
+            // @ts-expect-error
+            model = scene.children.at(-1)!.geometry;
+            model.scale(0.5, 0.5, 0.5);
         }
-    });
+    );
 
     // Appliqué les mouvement survenu sur les autres clients
     socket.on("mouvement", (Δ: CANNON.Vec3) => {
@@ -59,48 +70,44 @@
 
 <PE.World gravity={[0, -9.81, 0]}>
     <SC.Canvas antialias background={new THREE.Color("skyblue")} fog={new THREE.FogExp2("skyblue", 0.1)} shadows>
-        <DirecionalPerspectiveCamera bind:direction />
-        <SC.OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 3} />
+        <DirectionalPerspectiveCamera bind:direction />
+        <SC.OrbitControls
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 3}
+            enableZoom={false}
+            enablePan={false}
+            enableDamping={true}
+            dampingFactor={0.5}
+        />
         <SC.AmbientLight intensity={0.5} />
         <SC.DirectionalLight intensity={0.8} position={[-2, 3, 2]} shadow={{ mapSize: [2048, 2048] }} />
-        <ContactMaterial />
 
         <!-- Floor -->
-        <SC.Mesh
-            geometry={new THREE.PlaneGeometry(50, 50)}
-            material={new THREE.MeshStandardMaterial({ map: floorTexture})}
-            rotation={[-Math.PI / 2, 0, 0]}
-            position={[0, -height / 2, 0]}
-            receiveShadow
-        />
-        <PE.Body rotation={[-Math.PI / 2, 0, 0]} position={[0, -height, 0]}>
+        <SC.Group position={[0, -grandeur / 2, 0]}>
+            <SC.Mesh
+                geometry={new THREE.PlaneGeometry(50, 50)}
+                material={new THREE.MeshStandardMaterial({ map: floorTexture })}
+                rotation={[-Math.PI / 2, 0, 0]}
+                receiveShadow
+            />
+            <SC.Primitive object={new THREE.GridHelper(50 * grandeur, 50 * grandeur, "saddlebrown", "sienna")} position={[0.5, 0.001, 0.5]} />
+        </SC.Group>
+        <PE.Body rotation={[-Math.PI / 2, 0, 0]} position={[0, -grandeur, 0]}>
             <PE.Plane />
         </PE.Body>
 
-        <!-- Box -->
-        <SC.Mesh
-            geometry={new THREE.BoxGeometry()}
-            material={new THREE.MeshStandardMaterial({ color })}
-            scale={[width, height, depth]}
-            position={$position.toArray()}
-            castShadow
-            receiveShadow
-        />
+        <!-- Pawn -->
+        <SC.Group scale={[grandeur, grandeur, grandeur]}>
+            <SC.Mesh
+                geometry={model}
+                material={new THREE.MeshStandardMaterial({ color })}
+                position={$position.toArray()}
+                castShadow
+                receiveShadow
+            />
+        </SC.Group>
         <PE.Body mass={10} bind:position={$position}>
-            <PE.Box size={[width, height, depth]} />
-        </PE.Body>
-
-        <!-- Obstacle -->
-        <SC.Mesh
-            geometry={new THREE.BoxGeometry()}
-            material={new THREE.MeshStandardMaterial({ color: "blue" })}
-            scale={[width / 2, height / 2, depth / 2]}
-            position={[2, -0.25, 0]}
-            castShadow
-            receiveShadow
-        />
-        <PE.Body mass={5} position={[2, -0.25, 0]}>
-            <PE.Box size={[width / 2, height / 2, depth / 2]} />
+            <PE.Box size={[grandeur, grandeur, grandeur]} />
         </PE.Body>
     </SC.Canvas>
 </PE.World>
